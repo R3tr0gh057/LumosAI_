0715b3d6f5a432467e7f678e9a6bc495e8b40a44
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-01-25 17:59:56 +0530

    Test folder for testing server

--- ESP_code.ino/ESP_code.ino.ino
+++ ESP_code.ino/ESP_code.ino.ino
@@ -1,28 +1,6 @@
-#include <ESP8266WiFi.h>
+void setup(){
 
-const char* ssid     = "";
-const char* password = "";
-
-void setup() {
-  Serial.begin(9600);
-  delay(10);
-  Serial.println(" ");
-  
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to ");
-  Serial.print(ssid); 
-  Serial.println(" ...");
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print("."); 
-    Serial.print(" ");
-  }
-
-  Serial.println(' ');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:");
-  Serial.println(WiFi.localIP());
 }
-
-void loop() { }
+void loop(){
+  
+}
