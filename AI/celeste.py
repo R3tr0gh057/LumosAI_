245ac386b396b46ce8265bc3ff5512ba1ac42118
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-01-29 02:15:43 +0530

    Updating the code to accommodate two leds

---
+++ AI/celeste.py
@@ -0,0 +1,64 @@
+import pyttsx3
+import speech_recognition as sr
+import requests
+import datetime
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning Toad!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon Toad!")   
+
+    else:
+        speak("Good Evening Toad!")  
+
+    speak("I am Celeste. How may I help you") 
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("[+] Listening...\n")
+        r.pause_threshold = 1
+        audio = r.listen(source, phrase_time_limit=None, timeout=None)
+
+    try:
+        print("[+] Recognizing...\n")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)    
+        print("[!] Say that again please...\n")  
+        return "None"
+    return query
+
+def main():
+    while True:
+        query = takeCommand().lower()
+        if 'jarvis' in query:
+            speak('How may I help you?')
+            query = takeCommand().lower()
+
+            if 'turn on the lights' in query:
+                op = requests.get('https://bulldog-promoted-accurately.ngrok-free.app/toggle-led')
+                speak(op.text)
+            elif 'turn off the lights' in query:
+                op = requests.get('https://bulldog-promoted-accurately.ngrok-free.app/toggle-led')
+                speak(op.text)
+            else:
+                speak('Could you say that again please?')
+                main()
+
+if __name__ == "__main__":
+    main()
--- CPP/CPP.ino
+++ CPP/CPP.ino
@@ -1,16 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char *ssid = "Redmi Note 10 Pro";
-const char *password = "12345687";
+const char *ssid = "Celeste_2.4";
+const char *password = "7907003596";
 
 ESP8266WebServer server(3000);
 
-int ledPin = D1;
+int ledPin1 = D1;
+int ledPin2 = D2;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, LOW);
   Serial.begin(9600);
 
   WiFi.begin(ssid, password);
@@ -22,8 +25,18 @@ void setup() {
   Serial.println("IP: ");
   Serial.print(WiFi.localIP());
 
-  server.on("/toggle-led", HTTP_GET, []() {
-      digitalWrite(ledPin, !digitalRead(ledPin));
+  server.on("/toggle-led1", HTTP_GET, []() {
+      digitalWrite(ledPin1, !digitalRead(ledPin1));
+      server.send(200, "text/plain", "LED Toggled");
+  });
+
+  server.on("/toggle-led2", HTTP_GET, []() {
+      digitalWrite(ledPin2, !digitalRead(ledPin2));
+      server.send(200, "text/plain", "LED Toggled");
+  });
+
+  server.on("/blink-led1", HTTP_GET, []() {
+      digitalWrite(ledPin1, !digitalRead(ledPin1));
       server.send(200, "text/plain", "LED Toggled");
   });
 
