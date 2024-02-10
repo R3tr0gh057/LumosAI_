35ca251b5b81cfcfe38c0eb58bb4c864971fd054
HPSingh7146 <hpsingh7146@gmail.com>
2024-02-10 23:16:31 +0530

    Added new html

---
+++ HTML_new/script.js
@@ -0,0 +1,73 @@
+const sideLinks = document.querySelectorAll('.sidebar .side-menu li a:not(.logout)');
+
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
+
+
+
+//Chart
+
+
+var options = {
+    series: [{
+    name: 'Sales',
+    data: [4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5]
+  }],
+    chart: {
+    height: 350,
+    type: 'line',
+  },
+  forecastDataPoints: {
+    count: 7
+  },
+  stroke: {
+    width: 5,
+    curve: 'smooth'
+  },
+  xaxis: {
+    type: 'datetime',
+    categories: ['1/11/2000', '2/11/2000', '3/11/2000', '4/11/2000', '5/11/2000', '6/11/2000', '7/11/2000', '8/11/2000', '9/11/2000', '10/11/2000', '11/11/2000', '12/11/2000', '1/11/2001', '2/11/2001', '3/11/2001','4/11/2001' ,'5/11/2001' ,'6/11/2001'],
+    tickAmount: 10,
+    labels: {
+      formatter: function(value, timestamp, opts) {
+        return opts.dateFormatter(new Date(timestamp), 'dd MMM')
+      }
+    }
+  },
+  title: {
+    text: 'Stats',
+    align: 'left',
+    style: {
+      fontSize: "16px",
+      color: '#666'
+    }
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      gradientToColors: [ '#9819d2'],
+      shadeIntensity: 1,
+      type: 'horizontal',
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 100, 100, 100]
+    },
+  },
+  yaxis: {
+    min: -10,
+    max: 40
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
+
