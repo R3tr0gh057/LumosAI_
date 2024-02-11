fd70057a930c0ba8f08ab8412a13b5896924866e
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-02-11 10:25:02 +0530

    login page fixed and finalized

--- Pages/Login/login.js
+++ Pages/Login/login.js
@@ -1,3 +1,68 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+/*FIREBASE AUTH*/
+
+const firebaseConfig = {
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const loginbutton = document.getElementById("submit-btn");
+const signupbutton = document.getElementById("submit-btn-su");
+const signupname = document.getElementById("signup-name");
+const signuppassword = document.getElementById("signup-passwd");
+const signupemail = document.getElementById("signup-email");
+const loginemail = document.getElementById("login-email");
+const loginpassword = document.getElementById("login-passwd")
+
+/*SIGNUP*/
+
+signupbutton.addEventListener('click', function(){
+  event.preventDefault();
+  var isVerified = true;
+  var mail = signupemail.value;
+  var pass = signuppassword.value;
+
+  if(isVerified) {
+    createUserWithEmailAndPassword(auth, mail, pass)
+      .then((userCredential) => {
+      const user = userCredential.user;
+      window.alert("Account created.");
+      console.log("Account created")
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert("Not a valid email id");
+      console.log(errorMessage);
+    });
+  }
+});
+
+/*LOGIN*/
+
+loginbutton.addEventListener('click', function(){
+  event.preventDefault();
+  var mail1 = loginemail.value;
+  var pass2 = loginpassword.value;
+  signInWithEmailAndPassword(auth, mail1, pass2)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("Signed in");
+      window.location.href = "D:/--PROJECTS2024--/LumosAI_/homepage.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert("Invalid Credentials");
+      console.log(errorMessage)
+    });
+});
+
+/*Animation*/
+
 const container = document.getElementById("container");
 const registerBtn = document.getElementById("register");
 const loginBtn = document.getElementById("login");
@@ -9,23 +74,3 @@ registerBtn.addEventListener("click", () => {
 loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  var loginBtn = document.getElementById("submit-btn");
-
-  loginBtn.addEventListener("click", function(event) {
-      event.preventDefault();
-      
-      var emailInput = document.getElementById("login-email").value;
-      var passwordInput = document.getElementById("login-passwd").value;
-
-      if (emailInput.includes("dedsec8080@gmail.com") && passwordInput.includes("toad")) {
-          window.location.href = "D:/--PROJECTS2024--/LumosAI_/homepage.html";
-      } else {
-          alert("Incorrect credentials");
-      }
-  });
-});
-
-/*FIREBASE AUTH*/
-
