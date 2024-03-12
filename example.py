ac97825fa9eb7a308bf4e83fe2f487515382f2a6
ANJITHA M P <156066666+anjithamp3@users.noreply.github.com>
2024-03-12 15:06:59 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 x = 10
 y = 15
-
+z=10
 # To take inputs from the user
 #x = input('Enter value of x: ')
 #y = input('Enter value of y: ')
@@ -8,7 +8,8 @@ y = 15
 # create a temporary variable and swap the values
 temp = x
 x = y
-y = temp
+y = tem
+z= x
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
