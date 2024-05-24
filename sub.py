f60f1f57aecd018a9939249d6b099a8b6ffdc196
taniya jaison <156457415+taniya221@users.noreply.github.com>
2024-05-24 22:29:39 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,7 +1,7 @@
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
-num = 8 
+num = 9 
 
 # To take the input from the user
 #num = float(input('Enter a number: '))
